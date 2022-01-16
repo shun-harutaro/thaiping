a9b330b29caa2d9b5ec5b51f5b82694d9460860c
shun_harutaro <shun_harutaro@protonmail.com>
2022-01-16 13:42:37 +0700

    create start and define component for controling view

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
 //import {css, keyframes} from '@emotion/react'
+import Start from './components/start'
 import Game from './components/game'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      window: 'start',
+    }
+  }
   render() {
-    return (
-      <div>
-        <Game />
-      </div>
-    )
+    const current_window = this.state.window;
+    if (current_window === 'start') {
+      return (<Start />)
+    } else if (current_window === 'game') {
+      return (<Game />)
+    }
   }
 };
---
+++ src/components/start.js
@@ -0,0 +1,33 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import {css, keyframes} from '@emotion/react'
+
+export default class Start extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div css={body}>
+        <div css={wrapper}>
+          <p>start window</p>
+        </div>
+      </div>
+    )
+  }
+};
+
+const body = css`
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: lightgray;
+`
+
+const wrapper = css`
+  background: white;
+  width: 50%;
+  text-align: center;
+`
